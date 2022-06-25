611fd71b00a42f3791b7245139e37ec71c7a65dd
llliue <1394415299@qq.com>
2022-06-25 13:12:22 +0800

    lew-test

--- ticketsquery-demo/src/main/java/team/seven/ticketsquery/TicketsqueryDemoApplication.java
+++ ticketsquery-demo/src/main/java/team/seven/ticketsquery/TicketsqueryDemoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class TicketsqueryDemoApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(TicketsqueryDemoApplication.class, args);
     }
